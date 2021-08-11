6900abb5bdd6df6e74d13df40d4b9e85d4bd60d9
JGuo <@jason.guo2560@gmail.com>
2021-08-11 15:14:01 -0400

    frontend-v0.22.1 - added tooltip to vision detail

--- src/components/match-components/overview/WardDetails.jsx
+++ src/components/match-components/overview/WardDetails.jsx
@@ -1,13 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TableDetailSection.css";
+import { usePopper } from "react-popper";
 
 export default function WardDetails(props) {
   const { wardsPlaced, wardsKilled, redsPlaced, visionScore } = props;
 
+  const [isVisible, setIsVisible] = useState(false);
+  const [referenceElement, setReferenceElement] = useState(null);
+  const [popperElement, setPopperElement] = useState(null);
+  const [arrowElement, setArrowElement] = useState(null);
+  const { styles, attributes } = usePopper(referenceElement, popperElement, {
+    placement: "top",
+    modifiers: [
+      { name: "arrow", options: { element: arrowElement } },
+      { name: "offset", options: { offset: [0, 8] } },
+      { name: "flip", options: { fallbackPlacements: ["top", "bottom"] } },
+    ],
+  });
+
   return (
-    <div className="table-detail">
-      <span>{visionScore}</span>
-      <span>{`${wardsPlaced} (${redsPlaced}) / ${wardsKilled}`}</span>
+    <div>
+      <div
+        className="table-detail"
+        ref={setReferenceElement}
+        onMouseOver={() => {
+          setIsVisible(true);
+        }}
+        onMouseLeave={() => {
+          setIsVisible(false);
+        }}
+      >
+        <span>{visionScore}</span>
+        <span>{`${wardsPlaced} (${redsPlaced}) / ${wardsKilled}`}</span>
+      </div>
+      {isVisible ? (
+        <div
+          className="popper-tooltip"
+          ref={setPopperElement}
+          style={styles.popper}
+          {...attributes.popper}
+        >
+          <div>
+            <p>Vision Score: {visionScore}</p>
+            <p>Total Wards Placed: {wardsPlaced}</p>
+            <p>Red Wards Placed: {redsPlaced}</p>
+            <p>Wards Killed: {wardsKilled}</p>
+          </div>
+          <div
+            className="popper-arrow"
+            ref={setArrowElement}
+            style={styles.arrow}
+          />
+        </div>
+      ) : null}
     </div>
   );
 }
