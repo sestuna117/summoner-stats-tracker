1c43de1db9d3348b3900e67035e04e8fde21745e
JGuo <@jason.guo2560@gmail.com>
2021-08-10 21:05:02 -0400

    frontend-v0.21.0 - implemented tooltip popup for items showing more detail

--- src/components/match-components/Item.jsx
+++ src/components/match-components/Item.jsx
@@ -1,8 +1,11 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { DDragonVersionContext, ItemDataContext } from "../../hook";
 import getItemIcon from "../../util/getItemIcon";
 import "./Item.css";
 import noItem from "../../icons/emptyslot.png";
+import { usePopper } from "react-popper";
+import "../PopperTooltip.css";
+import ItemTooltip from "./ItemTooltip";
 
 export default function Item(props) {
   const { itemId } = props;
@@ -14,6 +17,28 @@ export default function Item(props) {
     (item) => parseInt(item[0]) === itemId
   );
 
+  useEffect(() => {
+    if (itemId) {
+      return;
+    }
+    itemInfo = Object.entries(itemData.data).find(
+      (item) => parseInt(item[0]) === itemId
+    );
+  }, [itemId]);
+
+  const [isVisible, setIsVisible] = useState(false);
+  const [referenceElement, setReferenceElement] = useState(null);
+  const [popperElement, setPopperElement] = useState(null);
+  const [arrowElement, setArrowElement] = useState(null);
+  const { styles, attributes } = usePopper(referenceElement, popperElement, {
+    placement: "top",
+    modifiers: [
+      { name: "arrow", options: { element: arrowElement } },
+      { name: "offset", options: { offset: [0, 8] } },
+      { name: "flip", options: { fallbackPlacements: ["top", "bottom"] } },
+    ],
+  });
+
   return isNoItem ? (
     <div className="item">
       <img className="item-sprite" src={noItem} alt={"No Item"} />
@@ -24,7 +49,30 @@ export default function Item(props) {
         className="item-sprite"
         src={getItemIcon(itemId, dDragon)}
         alt={`${itemInfo[1].name}`}
+        ref={setReferenceElement}
+        onMouseOver={() => {
+          setIsVisible(true);
+        }}
+        onMouseLeave={() => {
+          setIsVisible(true);
+        }}
       />
+
+      {isVisible ? (
+        <div
+          className="popper-tooltip"
+          ref={setPopperElement}
+          style={styles.popper}
+          {...attributes.popper}
+        >
+          <ItemTooltip itemInfo={itemInfo[1]} />
+          <div
+            className="popper-arrow"
+            ref={setArrowElement}
+            style={styles.arrow}
+          />
+        </div>
+      ) : null}
     </div>
   );
 }
---
+++ src/components/match-components/ItemTooltip.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./ItemTooltip.css";
+
+// const BREAK = /(<br>)/;
+const BRACKETS = /<.*?>/g;
+
+export default function ItemTooltip(props) {
+  const { itemInfo } = props;
+  const { name, plaintext, description, gold } = itemInfo;
+
+  let itemDesc = description.split("<br>");
+
+  return (
+    <div className="item-tooltip">
+      <p className="item-tooltip-name">{name}</p>
+      <p className="item-tooltip-text">{plaintext}</p>
+      <div className="item-tooltip-desc">
+        {itemDesc.map((desc) => (
+          <p>
+            {desc.replace(BRACKETS, "")}
+            <br />
+          </p>
+        ))}
+      </div>
+      {gold.total !== 0 && gold.sell !== 0 ? (
+        <div>
+          <span>Gold: </span>
+          <span className="item-tooltip-gold">{`${gold.total} (${gold.sell})`}</span>
+        </div>
+      ) : null}
+    </div>
+  );
+}
